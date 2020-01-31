f0ba57fbf069939fc9d9edb20d7a84e34e3643f5
Matheus <matheusphranco@gmail.com>
2020-01-30 23:03:42 -0300

    Create the scraping model and set the correct timezone

--- scraping/main/models.py
+++ scraping/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Scraping(models.Model):
+    class Meta:
+        db_table = 'Scraping'
+
+    title = models.CharField(max_length=500, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
