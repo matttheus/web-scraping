dd253eb31ac66e25f774f8ddf62f9604c8d39e90
Matheus <matheusphranco@gmail.com>
2020-02-01 17:15:49 -0300

    Implement search functionality and fix url namespace

--- scraping/main/urls.py
+++ scraping/main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from main.views import ListScrapingView
+from main.views import (ListScrapingView, SearchScrapingView,)
 
-app_web = 'main'
+app_name = 'main'
 
 urlpatterns = [
     path('', ListScrapingView.as_view(), name='list-scraping'),
+    path('search/', SearchScrapingView.as_view(), name='search-scraping'),
 ]
--- scraping/main/views.py
+++ scraping/main/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from main.models import Scraping
 
+
 class ListScrapingView(ListView):
     model = Scraping
     paginate_by = 10
     context_object_name = 'scrapings'
+
+
+class SearchScrapingView(ListView):
+    model = Scraping
+    paginate_by = 10
+    context_object_name = 'scrapings'
+
+    def get_queryset(self):
+        query = self.request.GET.get('search')
+        print(query)
+        scrapings = Scraping.objects.filter(title__contains=query)
+        return scrapings
