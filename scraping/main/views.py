d853dcbfd65c1e82d8f530b540acd3e3198e0c26
Matheus <matheusphranco@gmail.com>
2020-02-01 20:35:17 -0300

    Fix bug of the page navigation

--- scraping/main/views.py
+++ scraping/main/views.py
@@ -15,6 +15,11 @@ class SearchScrapingView(ListView):
     context_object_name = 'scrapings'
 
     def get_queryset(self):
-        query = self.request.GET.get('search')
+        query = self.request.GET.get('q')
         scrapings = Scraping.objects.filter(title__contains=query)
         return scrapings
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['params'] = self.request.GET.get('q')
+        return context
